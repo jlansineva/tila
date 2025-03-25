460f3785a55ff5304dd5f3f0075da2bb9813b2e1
jukkalansineva <westijt@gmail.com>
2025-03-25 23:01:35 +0200

    adding some new fun macros and other util functions, plus improving tests

--- src/pelinrakentaja_tila/utils.clj
+++ src/pelinrakentaja_tila/utils.clj
@@ -5,7 +5,7 @@
   [states state-name]
   {:pre [(contains? states state-name)
          (keyword? state-name)]}
-  {state-name (get states state-name)})
+  {state-name (get states state-name)}) ;TESTED
 
 (defn get-states
   [fsm-struct]
@@ -28,12 +28,12 @@
    (add-effect effect-name effect-fn {:protected? true}))
   ([effect-name effect-fn {:keys [protected?]}]
   {:pre [(keyword? effect-name)
-         (fn? effect-fn)]}
-   (let [existing-with-name (get @core/effects effect-name)]
-     (if (and (some? existing-with-name)
-              protected?)
-       (prn :error> "attempting to overwrite existing effect")
-       (swap! core/effects assoc effect-name effect-fn)))))
+         (fn? effect-fn)]} 
+   (let [existing-with-name (get @core/effects effect-name)] 
+     (assert (not (and (some? existing-with-name)
+                       protected?))
+             "Attempting to overwrite existing effect")
+     (swap! core/effects assoc effect-name effect-fn)))) ;TESTED
 
 (defn add-evaluation
   ([evaluation-name evaluation-fn]
@@ -42,10 +42,10 @@
   {:pre [(keyword? evaluation-name)
          (fn? evaluation-fn)]}
   (let [existing-with-name (get @core/evaluations evaluation-name)]
-    (if (and (some? existing-with-name)
-             protected?)
-      (prn :error> "attempting to overwrite existing evaluation")
-      (swap! core/evaluations assoc evaluation-name evaluation-fn)))))
+    (assert (not (and (some? existing-with-name)
+                      protected?))
+            "Attempting to overwrite existing evaluation")
+    (swap! core/evaluations assoc evaluation-name evaluation-fn)))) ;TESTED
 
 (comment 
   (add-state states 
@@ -83,24 +83,25 @@
          #_state-has-at-least-one-effect?
          state-effects-are-right-form?
          state-transitions-is-a-vector?
-         state-transitions-are-right-form?)))
+         state-transitions-are-right-form?))) ;TESTED
 
 (defn new-state? 
   [states state]
   (let [new-state-key (first (keys state))]
-    (not (contains? states new-state-key))))
+    (not (contains? states new-state-key)))) ;TESTED
 
 (defn add-state
   [states new-state]
   {:pre [(valid-state? new-state)
-         (new-state? states new-state)]}
+         (new-state? states new-state)]} 
+  (prn :states-in-add states)
   (merge states new-state))
 
 (defn with-state 
   [state-name]
   {:pre [(keyword? state-name)]}
   {state-name {:effect nil
-               :transitions []}})
+               :transitions []}}) ;TESTED
 
 (defn with-effect
   "Adds an effect to a state. If an effect exists and it is "
@@ -125,21 +126,22 @@
                                      (true?
                                       (some #(when (= % effect) true) existing-effect))
                                      (= effect existing-effect))]
-       {state-name
-        (cond
-          (and multi-effect?
-               (not adding-existing-effect?))
-          (update state-content :effect conj effect)
+       (merge states
+              {state-name
+               (cond
+                 (and multi-effect?
+                      (not adding-existing-effect?))
+                 (update state-content :effect conj effect)
 
-          (and (not multi-effect?)
-               (not adding-existing-effect?)
-               (some? existing-effect))
-          (assoc state-content :effect [(:effect state-content) effect])
+                 (and (not multi-effect?)
+                      (not adding-existing-effect?)
+                      (some? existing-effect))
+                 (assoc state-content :effect [(:effect state-content) effect])
 
-          (and (not multi-effect?)
-               (not adding-existing-effect?)
-               (nil? existing-effect))
-          (assoc state-content :effect effect))}))))
+                 (and (not multi-effect?)
+                      (not adding-existing-effect?)
+                      (nil? existing-effect))
+                 (assoc state-content :effect effect))}))))) ;TESTED
 
 (defn with-transition
   [state transition-state & transition-evaluations]
@@ -166,11 +168,11 @@
 
 (defn list-effects
   []
-  (keys @core/effects))
+  (vec (keys @core/effects))) ;TESTED
 
 (defn list-evaluations 
   []
-  (keys @core/evaluations))
+  (vec (keys @core/evaluations))) ;TESTED
 
 (defn -search
   [search-sequence pattern]
@@ -186,22 +188,22 @@
   (-search (list-evaluations) search-pattern))
 
 (defn replace-effect
-  [state effect new-effect]
-  {:pre [(valid-state? state)
+  [states state-to-modify effect new-effect]
+  {:pre [(valid-state? states)
          (keyword? effect)
          (keyword? new-effect)]}
-  (let [multi-effect? (vector? (:effect state))]
-    (assoc state
-           :effect
+  (let [multi-effect? (vector? (:effect (get states state-to-modify)))]
+    (assoc-in states
+           [state-to-modify :effect]
            (if multi-effect?
-             (mapv (fn [-effect]
+             (mapv (fn [-effect] 
                      (if (= effect -effect)
                        new-effect
-                       effect))
-                   (:effect state))
-             (if (= effect (:effect state))
+                       -effect))
+                   (get-in states [state-to-modify :effect]))
+             (if (= effect (get-in states [state-to-modify :effect]))
                new-effect
-               effect)))))
+               effect))))) ;TESTED
 
 
 (defn update-state-in-fsm
@@ -212,11 +214,18 @@
       (assoc-in fsm-struct (get-state-path old-state) new-state)
       fsm-struct)))
 
+(defn add-state-to-fsm
+  [fsm-struct new-state] 
+  (update fsm-struct :states add-state new-state))
+
+(defn remove-state-from-fsm
+  [fsm-struct state-name])
+
 (defn overwrite-effect
   [state state-to-modify new-effect]
   {:pre [(valid-state? state)
          (keyword? new-effect)]} 
-  (assoc-in state [state-to-modify :effect] new-effect))
+  (assoc-in state [state-to-modify :effect] new-effect)) ;TESTED
 
 (defn remove-effect
   [states state-to-modify removed-effect]
@@ -232,6 +241,36 @@
             updated-effect (if (= 1 (count with-effect-removed))
                              (first with-effect-removed)
                              with-effect-removed)]
-        (assoc-in states [state-name :effect] updated-effect))
-      (throw (java.lang.AssertionError. "Can't remove the last effect, use replace-effect or overwrite-effect")))))
+        (assoc-in states [state-name :effect] updated-effect)) 
+      (throw (java.lang.AssertionError. "Can't remove the last effect, use replace-effect or overwrite-effect"))))) ;TESTED
 
+(defn create-fsm
+  []
+  {:pre {}
+   :always []
+   :current {}
+   :require []
+   :states {}})
+
+(defn operate-fsm-on->
+  [fsm key bodies]
+  `(update ~fsm ~key (fn [~'states] (-> ~'states ~@bodies))))
+
+(defmacro update-states-in-fsm->
+  [fsm & bodies]
+  (operate-fsm-on-> fsm :states bodies))
+
+(defmacro update-requires-in-fsm->
+  [fsm & bodies]
+  (operate-fsm-on-> fsm :require bodies))
+
+(defmacro on-state->
+  [state-key & bodies]
+  `(fn [~'state]
+     ~(loop [body (first bodies)
+             bodies (rest bodies)
+             final '()]
+        (if-not (some? body)
+          final
+          (recur (first bodies)
+                 (rest bodies) (concat final (list (concat (list (first body) state-key) (rest body)))))))))
--- test/pelinrakentaja_tila/utils_test.clj
+++ test/pelinrakentaja_tila/utils_test.clj
@@ -1,6 +1,7 @@
 (ns pelinrakentaja-tila.utils-test
   (:require [clojure.test :refer :all]
-            [pelinrakentaja-tila.utils :refer :all]))
+            [pelinrakentaja-tila.utils :refer :all]
+            [pelinrakentaja-tila.core :as core]))
 
 (def -state-object {:sleeping {:effect :sleep
                                :transitions [{:when [:woke-up :alarm-rings :clock-is-9]
@@ -17,11 +18,16 @@
                        :transitions [{:when [:woke-up :alarm-rings :clock-is-9]
                                       :switch :drinking}
                                      {:when [:woke-up :still-night]
-                                      :switch :go-back-to-sleep}]}} 
+                                      :switch :go-back-to-sleep}]}}
            (get-state-from-states state-object :sleeping)))
     (is (thrown? java.lang.AssertionError (get-state-from-states state-object :does-not-exist)))))
 
-(deftest new-state?-test 
+(deftest check-state-transitions-test
+  (let [good-transition {:when [:alive :more-alive]}]
+  (testing "Good transitions")
+  (testing "Bad transitions")))
+
+(deftest new-state?-test
   (let [state-object -state-object]
     (testing "Given state is new to existing state object"
       (is (true? (new-state? state-object {:running {}}))))
@@ -42,7 +48,7 @@
         number-multi-effect-state (update proper-state-with-multi-effect :effect conj 666)]
     (testing "State name is not a keyword caught"
       (is (false? (valid-state? {"running" proper-state-content})))
-      (is (false? (valid-state? {42 proper-state-content}))) 
+      (is (false? (valid-state? {42 proper-state-content})))
       (is (true? (valid-state? {:running proper-state-content}))))
     (testing "State doesn't have effects"
       (is (true? (valid-state? {:running no-effect-state}))))
@@ -59,19 +65,38 @@
 (deftest with-state-test
   (testing "Creation of a new state"
     (is (map? (with-state :sleeping)))
+    (is (= {:sleeping {:effect nil :transitions []}} (with-state :sleeping)))
     (is (thrown? java.lang.AssertionError (with-state "sixsixsix")))
     (is (thrown? java.lang.AssertionError (with-state 9999)))
     (is (thrown? java.lang.AssertionError (with-state nil)))
     (is (thrown? java.lang.AssertionError (with-state {})))))
 
-(deftest with-effect-test 
+(deftest add-state-test
+  (testing "Adding a new state to states"
+    (let [state-object -state-object]
+      (is (= {:sleeping {:effect :sleep
+                         :transitions [{:when [:woke-up :alarm-rings :clock-is-9]
+                                        :switch :drinking}
+                                       {:when [:woke-up :still-night]
+                                        :switch :go-back-to-sleep}]}
+              :drinking {:effect :drink
+                         :transitions [{:when [:had-too-many]
+                                        :switch :sleeping}]}
+              :eating {:effect nil
+                       :transitions []}}
+             (add-state state-object (with-state :eating))))
+      (testing "Adding existing state fails"
+        (is (thrown? java.lang.AssertionError
+                     (add-state state-object (with-state :sleeping))))))))
+
+(deftest with-effect-test
   (let [state-object -state-object]
     (testing "Adding an effect"
       (is (= {:sleeping {:effect [:sleep :growl]
-                        :transitions [{:when [:woke-up :alarm-rings :clock-is-9]
-                                       :switch :drinking}
-                                      {:when [:woke-up :still-night]
-                                       :switch :go-back-to-sleep}]}} 
+                         :transitions [{:when [:woke-up :alarm-rings :clock-is-9]
+                                        :switch :drinking}
+                                       {:when [:woke-up :still-night]
+                                        :switch :go-back-to-sleep}]}}
              (with-effect state-object :sleeping :growl)))
       (is (= {:sleeping {:effect :growl
                          :transitions [{:when [:woke-up :alarm-rings :clock-is-9]
@@ -90,10 +115,10 @@
                                         :switch :drinking}
                                        {:when [:woke-up :still-night]
                                         :switch :go-back-to-sleep}]}}
-             (-> state-object 
+             (-> state-object
                  (with-effect :sleeping :growl)
                  (with-effect :sleeping :drool))))
-      (is (thrown? java.lang.AssertionError 
+      (is (thrown? java.lang.AssertionError
                    (with-effect (:sleeping state-object) :sleeping :growl))))))
 
 (deftest remove-effect-test
@@ -120,6 +145,170 @@
                                           :switch :go-back-to-sleep}]}
                 :drinking {:effect :drink
                            :transitions [{:when [:had-too-many]
-                                          :switch :sleeping}]}} 
-               (overwrite-effect state-object :sleeping :has-nightmares))))))
-  )
+                                          :switch :sleeping}]}}
+               (overwrite-effect state-object :sleeping :has-nightmares)))))))
+
+(deftest replace-effect-test
+  (let [state-object (assoc-in -state-object [:drinking :effect] [:drink :drink-moooore])]
+    (testing "Replacing effect")
+    (is (= (replace-effect state-object :sleeping :sleep :wake-up)
+           {:sleeping {:effect :wake-up
+                       :transitions [{:when [:woke-up :alarm-rings :clock-is-9]
+                                      :switch :drinking}
+                                     {:when [:woke-up :still-night]
+                                      :switch :go-back-to-sleep}]}
+            :drinking {:effect [:drink :drink-moooore]
+                       :transitions [{:when [:had-too-many]
+                                      :switch :sleeping}]}}))
+    (is (= (replace-effect state-object :drinking :drink :abstain)
+           {:sleeping {:effect :sleep
+                       :transitions [{:when [:woke-up :alarm-rings :clock-is-9]
+                                      :switch :drinking}
+                                     {:when [:woke-up :still-night]
+                                      :switch :go-back-to-sleep}]}
+            :drinking {:effect [:abstain :drink-moooore]
+                       :transitions [{:when [:had-too-many]
+                                      :switch :sleeping}]}}))))
+
+(deftest list-effects-test
+  (with-redefs [pelinrakentaja-tila.core/effects (atom {:effect-a (constantly true) :effect-b (constantly false)})]
+    (is (= [:effect-a :effect-b] (list-effects)))))
+
+(deftest list-evaluations-test
+  (with-redefs [pelinrakentaja-tila.core/evaluations (atom {:evaluation-a (constantly true) :evaluation-b (constantly false)})]
+    (is (= [:evaluation-a :evaluation-b] (list-evaluations)))))
+
+(deftest add-effect-test
+  (let [initial-fn (fn [_self _required _state] :initial)
+        replaced-fn (fn [_self _required _state] :replaced)]
+    (with-redefs [pelinrakentaja-tila.core/effects (atom {})]
+      (testing "Adding effect"
+        (add-effect :burning-desire initial-fn)
+        (is (= :burning-desire (first (keys @pelinrakentaja-tila.core/effects))))
+        (is (= :initial ((:burning-desire @pelinrakentaja-tila.core/effects) :self :required :state))))
+      (testing "Adding existing evaluation"
+        (is (thrown? java.lang.AssertionError
+                     (add-effect :burning-desire replaced-fn)))
+        (is (= :initial ((:burning-desire @pelinrakentaja-tila.core/effects) :self :required :state))))
+      (testing "Adding existing evaluation, with protection override"
+        (is (= :initial ((:burning-desire @pelinrakentaja-tila.core/effects) :self :required :state)))
+        (add-effect :burning-desire replaced-fn {:protected? false})
+        (is (= :replaced ((:burning-desire @pelinrakentaja-tila.core/effects) :self :required :state))))
+      (testing "Adding multiple effects"
+        (reset! pelinrakentaja-tila.core/effects {})
+        (add-effect :burning-desire-1 initial-fn)
+        (add-effect :burning-desire-2 initial-fn)
+        (add-effect :burning-desire-3 initial-fn)
+        (add-effect :burning-desire-4 initial-fn)
+        (add-effect :burning-desire-5 initial-fn)
+        (is (= 5 (count (keys @pelinrakentaja-tila.core/effects))))))))
+
+(deftest add-evaluation-test
+  (let [initial-fn (fn [_state] :initial)
+        replaced-fn (fn [_state] :replaced)]
+    (with-redefs [pelinrakentaja-tila.core/evaluations (atom {})]
+      (testing "Adding evaluation"
+        (add-evaluation :has-burning-desire initial-fn)
+        (is (= :has-burning-desire (first (keys @pelinrakentaja-tila.core/evaluations))))
+        (is (= :initial ((:has-burning-desire @pelinrakentaja-tila.core/evaluations) :state))))
+      (testing "Adding existing evaluation"
+        (is (thrown? java.lang.AssertionError
+                     (add-evaluation :has-burning-desire replaced-fn)))
+        (is (= :initial ((:has-burning-desire @pelinrakentaja-tila.core/evaluations) :state))))
+      (testing "Adding existing evaluation with protection override"
+        (is (= :initial ((:has-burning-desire @pelinrakentaja-tila.core/evaluations) :state)))
+        (add-evaluation :has-burning-desire replaced-fn {:protected? false})
+        (is (= :replaced ((:has-burning-desire @pelinrakentaja-tila.core/evaluations) :state))))
+      (testing "Adding multiple evaluations"
+        (reset! pelinrakentaja-tila.core/evaluations {})
+        (add-evaluation :burning-desire-1 initial-fn)
+        (add-evaluation :burning-desire-2 initial-fn)
+        (add-evaluation :burning-desire-3 initial-fn)
+        (add-evaluation :burning-desire-4 initial-fn)
+        (add-evaluation :burning-desire-5 initial-fn)
+        (is (= 5 (count (keys @pelinrakentaja-tila.core/evaluations))))))))
+
+(deftest macro-test
+  (testing "update-states-in-fsm macro"
+    (is (= {:pre {}
+            :always []
+            :current {}
+            :require []
+            :states {:dreaming {:effect :to-dream
+                                :transitions []}}}
+           (update-states-in-fsm->
+            (create-fsm)
+            (add-state (with-state :dreaming))
+            (with-effect :dreaming :to-dream)))))
+  (testing "on-state-> macro"
+    (is )))
+
+#_(def guard-evaluations
+    {::attack-done attack-done
+     ::dead dead?
+     ::player-too-far player-too-far
+     ::player-at-attack-distance player-at-attack-distance
+     ::player-at-chase-distance player-at-chase-distance
+     ::chase-target chase-target})
+
+#_(def guard-effects
+    {::commit-attack commit-attack
+     ::chase-player chase-player
+     ::seek-target seek-target
+     ::queue-for-removal queue-for-removal
+     ::attack-cooling-down attack-cooling-down
+     ::invincibility-frames-countdown invincibility-frames-countdown})
+
+#_(def guard-affections
+    {:hurt hurt})
+
+(def guard-fsm
+  {:pre {:transitions [{:when [::dead]
+                        :switch :dead}]}
+   :always [::invincibility-frames-countdown] ;; TODO: Does nothing
+   :current {:state :idle :effect ::seek-target}
+   :require [:clock :player]
+   :states {:chasing-player {:effect [::chase-player ::invincibility-frames-countdown]
+                             :transitions [{:when [::player-too-far]
+                                            :switch :idle}
+                                           {:when [::player-at-attack-distance]
+                                            :switch :attacking-player
+                                            :post-effect ::commit-attack}]}
+            :dead {:effect ::queue-for-removal
+                   :transitions []}
+            :idle {:effect [::seek-target ::invincibility-frames-countdown]
+                   :transitions [{:when [::player-at-chase-distance ::chase-target]
+                                  :switch :chasing-player}]}
+            :attacking-player {:effect ::attack-cooling-down
+                               :transitions [{:when [::player-at-chase-distance ::attack-done]
+                                              :switch :chasing-player}]}}})
+
+(deftest update-state-in-fsm-test
+  (testing "updating a state in FSM"))
+
+(deftest add-state-to-fsm-test
+  (testing "Added a state to the FSM"
+    (let [new-state  {:dead {:effect ::queue-for-removal
+                             :transitions []}}
+          fsm (create-fsm)]
+      (is (= :a (add-state-to-fsm fsm new-state))))))
+
+(deftest endgame-test
+  (testing "Constructs an entity with all the functions"
+    (is (= guard-fsm (-> (create-fsm) (add-state-to-fsm (with-state :chasing-player))))))
+  (testing "Constructs an entity with all the macros"
+    (let [built-fsm (create-fsm)
+          built-fsm-with-states (update-states-in-fsm-> 
+                                 built-fsm
+                                 (add-state (with-state :chasing-player))
+                                 (on-state-> :chasing-player
+                                  (with-effect :chasing-player ::chase-player)
+                                  (with-effect :chasing-player ::invincibility-frames-countdown))
+                                 (add-state (with-state :dead))
+                                 (with-effect :dead ::queue-for-removal)
+                                 (add-state (with-state :idle))
+                                 (with-effect :idle ::seek-target)
+                                 (with-effect :idle ::invincibility-frames-countdown)
+                                 (add-state (with-state :attacking-player))
+                                 (with-effect :attacking-player ::attack-cooling-down))]
+      (is (= guard-fsm built-fsm-with-states)))))
